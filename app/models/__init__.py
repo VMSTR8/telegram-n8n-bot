fdff32912c966470d3000440f9682a6a18843040
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-20 01:06:32 +0300

    add models to module exports in __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,12 @@
+from .user import User, UserRole
+from .survey import Survey
+from .penalty import Penalty
+from .chat import Chat
+
+__all__ = [
+    'User',
+    'UserRole',
+    'Survey',
+    'Penalty',
+    'Chat'
+]
