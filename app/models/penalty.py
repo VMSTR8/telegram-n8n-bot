d6779b393dd9f6648f605738f8d115c6da165574
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-20 01:11:09 +0300

    add Penalty model to track user penalties for survey participation

---
+++ app/models/penalty.py
@@ -0,0 +1,27 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class Penalty(Model):
+
+    id = fields.IntField(pk=True)
+    user = fields.ForeignKeyField(
+        'models.User',
+        related_name='penalties',
+        description='Пользователь, которому назначен штрафной балл'
+    )
+    survey = fields.ForeignKeyField(
+        'models.Survey',
+        related_name='penalties',
+        description='Опрос, за который назначен штрафной балл'
+    )
+    reason = fields.TextField(description='Причина назначения штрафного балла')
+    penalty_date = fields.DatetimeField(auto_now_add=True, description='Дата и время назначения штрафного балла')
+
+    class Meta:
+        table = 'penalties'
+        table_description = 'Штрафные баллы пользователей за неучастие в опросах'
+
+    def __str__(self) -> str:
+        """Строковое представление штрафного балла"""
+        return f'Penalty {self.id} for User {self.user} in Survey {self.survey}'
