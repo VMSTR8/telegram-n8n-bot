4ebc6361bafed57d474c88b4af881cae4750e302
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-21 01:24:40 +0300

    add schemas to __init__.py for user, chat, survey, and penalty

---
+++ app/api_fastapi/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .user import UserRole, UserSchema
+from .chat import ChatSchema
+from .survey import SurveySchema
+from .penalty import PenaltySchema
+
+__all__ = [
+    "UserRole",
+    "UserSchema",
+    "ChatSchema",
+    "SurveySchema",
+    "PenaltySchema",
+]
