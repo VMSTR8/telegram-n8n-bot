aad1bdb3bf9446bc1419c4509b0bb39f848b5566
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-21 02:06:13 +0300

    refactor main.py to implement a main function and improve logging for bot startup

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import logging
 from asyncio import run
 from app.bot_telegram.bot import BotManager
 from app.bot_telegram.logging import setup_logging
 
-# ТЕСТОВЫЙ ЗАПУСК БОТА
-if __name__ == "__main__":
+
+def main() -> None:
+    """
+    Так! Это все пока пишется и разрабатывается.
+    Когда апишку напишу, то и тут нормальный main сделаю.
+    Нечего тут смотреть и читать докстринг этот.
+    Кыш отседа.
+    Иди свой проект делай.
+    Ну или не делай. Можешь и тут посидеть.
+    Зачем я пишу это все?
+    Да просто так.
+    Лишь бы мне не писать код. Ты же тоже тут,
+    потому что не хочешь писать код, а хочешь читать докстринги.
+    Вот и я так же.
+    Пойдем лучше код писать. (или нет)
+    """
     setup_logging()
-    start = BotManager()
-    run(start.start_polling())
+    logging.info('Бот запускается в development режиме...')
+
+    bot = BotManager()
+
+    run(bot.start_polling())
+
+
+if __name__ == "__main__":
+    main()
