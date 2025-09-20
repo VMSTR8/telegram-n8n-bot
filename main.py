a1f309e54d38852b287333afc7211708492d14fc
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-21 01:25:23 +0300

    initialize logging setup in main.py for improved logging management

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from app.bot_telegram.bot import BotManager
 from asyncio import run
+from app.bot_telegram.bot import BotManager
+from app.bot_telegram.logging import setup_logging
 
 # ТЕСТОВЫЙ ЗАПУСК БОТА
 if __name__ == "__main__":
+    setup_logging()
     start = BotManager()
     run(start.start_polling())
