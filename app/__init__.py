26628edb18f23f15388faf6d37c449d48458f7aa
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-21 02:12:07 +0300

    add __init__.py with bot description, version, and author information

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+"""
+Telegram Bot для автоматизации работы с опросами
+с интеграцией Google Api (Google Forms, Google Sheets) и сервиса n8n
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Maxim VMSTR8 Vinokurov'
+__description__ = ('Telegram Bot для автоматизации работы с опросами с '
+                   'интеграцией Google Api (Google Forms, Google Sheets) и сервиса n8n')
