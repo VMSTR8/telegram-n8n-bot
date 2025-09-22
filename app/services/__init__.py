6db4ade5e70a3989a071c9394881ceb4068dbdbf
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-23 02:19:36 +0300

    add service imports to __init__.py for user, chat, survey, and penalty services

---
+++ app/services/__init__.py
@@ -0,0 +1,11 @@
+from .user_service import UserService
+from .chat_service import ChatService
+from .survey_service import SurveyService
+from .penalty_service import PenaltyService
+
+__all__ = [
+    "UserService",
+    "ChatService",
+    "SurveyService",
+    "PenaltyService",
+]
