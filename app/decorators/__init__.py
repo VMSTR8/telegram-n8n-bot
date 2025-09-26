02dca9276421ed2c0c2604d02e3bde3026243e62
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-26 04:31:11 +0300

    feat: add required_chat_bind to auth imports and __all__ exports

--- app/decorators/__init__.py
+++ app/decorators/__init__.py
@@ -1,9 +1,15 @@
-from .auth import required_admin, required_creator, required_user_registration
+from .auth import (
+    required_admin,
+    required_creator,
+    required_user_registration,
+    required_chat_bind
+)
 from .validate import validate_callsign_create
 
 __all__ = [
     'required_creator',
     'required_admin',
     'required_user_registration',
+    'required_chat_bind',
     'validate_callsign_create',
 ]
