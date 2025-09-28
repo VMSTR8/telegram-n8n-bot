3fbf91663767d38bda8aac0a00a7b3dd7c1d97eb
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-29 02:44:47 +0300

    feat: add SystemHandlers to module exports

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,7 +1,9 @@
 from .user_handlers import UserHandlers
 from .admin_handlers import AdminHandlers
+from .system_handlers import SystemHandlers
 
 __all__ = [
     'UserHandlers',
     'AdminHandlers',
+    'SystemHandlers',
 ]
