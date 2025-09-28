074526ab7254c533b24ddd4f3c948f9185c773ba
Maxim Vinokurov <vmstr8@yandex.ru>
2025-09-29 01:27:53 +0300

    feat: update module exports and improve project description in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 """
-Telegram Bot для автоматизации работы с опросами
-с интеграцией Google Api (Google Forms, Google Sheets) и сервиса n8n
+Telegram Bot for automating survey work
+with integration of Google Api (Google Forms, Google Sheets) and n8n service
 """
 
 __version__ = '1.0.0'
 __author__ = 'Maxim VMSTR8 Vinokurov'
-__description__ = ('Telegram Bot для автоматизации работы с опросами с '
-                   'интеграцией Google Api (Google Forms, Google Sheets) и сервиса n8n')
+__description__ = (
+    'Telegram Bot for automating survey work with '
+    'integration of Google Api (Google Forms, Google Sheets) and n8n service'
+)
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,11 +1,12 @@
 from .user_service import UserService
-from .chat_service import ChatService
+from .chat_service import ChatService, ChatAlreadyBoundError
 from .survey_service import SurveyService
 from .penalty_service import PenaltyService
 
 __all__ = [
     "UserService",
     "ChatService",
+    "ChatAlreadyBoundError",
     "SurveyService",
     "PenaltyService",
 ]
