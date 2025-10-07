ff13612e1a959f899253ea9f44c8ecf896ce6e75
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-08 01:46:29 +0300

    fix: update Survey model to remove description and add expired flag

--- app/api_fastapi/schemas/survey.py
+++ app/api_fastapi/schemas/survey.py
@@ -9,11 +9,10 @@ class SurveySchema(BaseModel):
     id: int
     google_form_id: str
     title: str
-    description: str | None = None
     form_url: str
-    is_sent: bool
-    created_at: datetime
-    ended_at: datetime
+    created_at: datetime = datetime.now()
+    ended_at: datetime = None
+    expired: bool = False
 
     class Config:
         orm_mode = True
@@ -22,10 +21,9 @@ class SurveySchema(BaseModel):
                 "id": 1,
                 "google_form_id": "1FAIpQLSfD2...",
                 "title": "Survey on User Satisfaction",
-                "description": "Please fill out this survey.",
                 "form_url": "https://docs.google.com/forms/d/e/1FAIpQLSfD2.../viewform",
-                "is_sent": True,
                 "created_at": "2023-01-01T12:00:00",
-                "ended_at": "2023-01-31T12:00:00"
+                "ended_at": "2023-01-31T12:00:00",
+                "expired": False
             }
         }
