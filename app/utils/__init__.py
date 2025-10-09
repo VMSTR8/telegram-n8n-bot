b5085bc37b72f5b2bdfe7d3e4a460f41b6f30ab1
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-09 04:52:40 +0300

    fix: add validate_datetime_format to imports and __all__ list in __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,6 @@
-from .validators import validate_callsign_format
+from .validators import validate_callsign_format, validate_datetime_format
 
 __all__ = [
-    "validate_callsign_format",
+    'validate_callsign_format',
+    'validate_datetime_format'
 ]
