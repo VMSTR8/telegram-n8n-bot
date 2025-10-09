c1a8a365ca93b845ac5d4302d8ec54cfa0b7529f
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-09 04:52:28 +0300

    fix: add send_and_pin_telegram_message to message_tasks import and __all__ list

--- app/celery_tasks/__init__.py
+++ app/celery_tasks/__init__.py
@@ -1,6 +1,7 @@
-from .message_tasks import send_telegram_message, send_bulk_messages
+from .message_tasks import send_telegram_message, send_bulk_messages, send_and_pin_telegram_message
 
 __all__ = [
-    "send_telegram_message",
-    "send_bulk_messages",
+    'send_telegram_message',
+    'send_bulk_messages',
+    'send_and_pin_telegram_message'
 ]
