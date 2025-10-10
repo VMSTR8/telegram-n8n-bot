e90287c3a95a142557703905ec897dd33a50b9b6
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-11 00:16:14 +0300

    refactor: enhance type annotations and improve logging setup in logging.py

--- app/bot_telegram/logging.py
+++ app/bot_telegram/logging.py
@@ -1,5 +1,5 @@
 import logging
-import logging.handlers
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 
@@ -7,7 +7,7 @@ def setup_logging() -> None:
     """
     Sets up logging configuration for the application.
     """
-    logs_dir = Path('logs')
+    logs_dir: Path = Path('logs')
     logs_dir.mkdir(exist_ok=True)
 
     console_format = logging.Formatter(
@@ -20,17 +20,17 @@ def setup_logging() -> None:
     )
 
     # Set up root logger
-    root_logger = logging.getLogger()
+    root_logger: logging.Logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
 
     # Set up console handler
-    console_handler = logging.StreamHandler()
+    console_handler: logging.StreamHandler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(console_format)
     root_logger.addHandler(console_handler)
 
     # Set up file handlers
-    file_handler = logging.handlers.TimedRotatingFileHandler(
+    file_handler: TimedRotatingFileHandler = TimedRotatingFileHandler(
         logs_dir / 'telegram_bot.log', when='midnight', backupCount=30, encoding='utf-8'
     )
     file_handler.setLevel(logging.INFO)
@@ -38,7 +38,7 @@ def setup_logging() -> None:
     root_logger.addHandler(file_handler)
 
     # Set up error handler
-    error_handler = logging.handlers.TimedRotatingFileHandler(
+    error_handler: TimedRotatingFileHandler = TimedRotatingFileHandler(
         logs_dir / 'telegram_bot_error.log', when='W0', backupCount=13, encoding='utf-8'
     )
     error_handler.setLevel(logging.ERROR)
@@ -46,7 +46,7 @@ def setup_logging() -> None:
     root_logger.addHandler(error_handler)
 
     # Suppress overly verbose logs from external libraries
-    aiogram_events_logger = logging.getLogger('aiogram')
+    aiogram_events_logger: logging.Logger = logging.getLogger('aiogram')
     aiogram_events_logger.setLevel(logging.WARNING)
 
     logging.info('Logging is set up.')
