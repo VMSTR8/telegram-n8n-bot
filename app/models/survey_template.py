b30461655902c01ff8476abaec558d4989bed84a
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-12 02:56:59 +0300

    feat: add SurveyTemplate model for survey template data storage

---
+++ app/models/survey_template.py
@@ -0,0 +1,18 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class SurveyTemplate(Model):
+    """
+    Model representing a survey template.
+    """
+    id = fields.IntField(pk=True, description='Primary key')
+    name = fields.CharField(max_length=255, unique=True, description='Name of the survey template')
+    json_content = fields.JSONField(description='JSON content of the survey template')
+
+    class Meta:
+        table = 'survey_templates'
+        table_description = 'Survey templates stored in JSON format'
+
+    def __str__(self) -> str:
+        return f'Survey template: {self.name} (ID: {self.id})'
