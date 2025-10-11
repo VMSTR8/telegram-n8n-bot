3154a57f0182bb6e714a0d656cbc3b7727a1b6e3
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-12 02:55:23 +0300

    refactor: add SurveyTemplate to module exports in __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,11 +2,13 @@ from .user import User, UserRole
 from .survey import Survey
 from .penalty import Penalty
 from .chat import Chat
+from .survey_template import SurveyTemplate
 
 __all__ = [
     'User',
     'UserRole',
     'Survey',
+    'SurveyTemplate',
     'Penalty',
     'Chat'
 ]
