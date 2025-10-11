484302ec4047547b2a11e3ed330556c34a8fa939
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-12 02:55:29 +0300

    refactor: add SurveyTemplateService to module exports in __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,13 +2,15 @@ from .user_service import UserService
 from .chat_service import ChatService, ChatAlreadyBoundError
 from .survey_service import SurveyService
 from .penalty_service import PenaltyService
+from .survey_template_service import SurveyTemplateService
 from .message_queue_service import MessageQueueService
 
 __all__ = [
-    "UserService",
-    "ChatService",
-    "ChatAlreadyBoundError",
-    "SurveyService",
-    "PenaltyService",
-    "MessageQueueService"
+    'UserService',
+    'ChatService',
+    'ChatAlreadyBoundError',
+    'SurveyService',
+    'PenaltyService',
+    'SurveyTemplateService',
+    'MessageQueueService'
 ]
