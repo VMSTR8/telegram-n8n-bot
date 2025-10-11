4217f96b712270949ee1697415308f12a9184b17
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-12 02:56:52 +0300

    feat: add SurveyTemplateSchema for survey template data validation

---
+++ app/api_fastapi/schemas/survey_template.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class SurveyTemplateSchema(BaseModel):
+    """
+    Schema for survey template data.
+    """
+    id: int
+    name: str
+    json_content: dict
+
+    class Config:
+        from_attributes = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Google Forms creation template",
+                "json_content":
+                    {
+                        "info": {
+                            "title": "{{title}}",
+                            "documentTitle": "{{ended_at}}"
+                        }
+                    }
+            }
+        }
