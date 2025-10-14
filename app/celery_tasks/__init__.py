3aca70774244832663d0c47526e72207abaebe06
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-14 03:39:10 +0300

    feat: add ban_user_from_chat to telegram_tasks and update __all__ list

--- app/celery_tasks/__init__.py
+++ app/celery_tasks/__init__.py
@@ -1,7 +1,13 @@
-from .message_tasks import send_telegram_message, send_bulk_messages, send_and_pin_telegram_message
+from .telegram_tasks import (
+    send_telegram_message,
+    send_bulk_messages,
+    send_and_pin_telegram_message,
+    ban_user_from_chat
+)
 
 __all__ = [
     'send_telegram_message',
     'send_bulk_messages',
-    'send_and_pin_telegram_message'
+    'send_and_pin_telegram_message',
+    'ban_user_from_chat'
 ]
