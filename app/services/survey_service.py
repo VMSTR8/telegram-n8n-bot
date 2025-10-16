3909879d26e0ee0f4144ea3672d3904144215a10
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-17 01:56:20 +0300

    feat: enhance type hints and documentation in survey_service.py for improved clarity and consistency

--- app/services/survey_service.py
+++ app/services/survey_service.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional, List
+from zoneinfo import ZoneInfo
 
 from app.models import Survey
 from config import settings
@@ -8,47 +8,38 @@ from config import settings
 class SurveyService:
     """
     Service for managing surveys.
+
+    Attributes:
+        tz (ZoneInfo): Timezone information from settings
+    
+    Methods:
+        get_survey_by_google_form_id: Retrieves a survey by its Google form ID.
+        get_active_surveys: Retrieves all active (not finished) surveys.
     """
 
     def __init__(self):
-        self.tz = settings.timezone_zoneinfo
-
-    @staticmethod
-    async def create_survey(
-            google_form_id: str,
-            title: str,
-            form_url: str,
-            ended_at: datetime,
-            description: Optional[str] = None,
-    ) -> Survey:
-        """
-        Creates a new survey.
-
-        :param google_form_id: Google form ID
-        :param title: Survey title
-        :param form_url: Google form URL
-        :param ended_at: Date and time when the survey ends
-        :param description: Survey description (optional)
-        :return: Survey - created Survey object
-        """
-        pass
+        self.tz: ZoneInfo = settings.timezone_zoneinfo
 
     @staticmethod
     async def get_survey_by_google_form_id(
             google_form_id: str
-    ) -> Optional[Survey]:
+    ) -> Survey | None:
         """
         Gets a survey by its Google form ID.
 
-        :param google_form_id: Google form ID
-        :return: Optional[Survey] - Survey object or None if not found
+        Args:
+            google_form_id (str): Google form ID of the survey.
+        
+        Returns:
+            Survey | None: Survey object if found, else None
         """
         return await Survey.filter(google_form_id=google_form_id).first()
 
-    async def get_active_surveys(self) -> List[Survey]:
+    async def get_active_surveys(self) -> list[Survey]:
         """
         Gets all active surveys (not finished).
 
-        :return: List[Survey] - List of active surveys
+        Returns:
+            list[Survey]: List of active Survey objects
         """
         return await Survey.filter(ended_at__gt=datetime.now(tz=self.tz)).all()
