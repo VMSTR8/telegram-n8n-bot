152cc187c9b90f6badf44fac154a333a7971a75d
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-18 03:30:55 +0300

    refactor: add __init__.py to expose celery and survey schemas for improved module structure

---
+++ app/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .celery_schemas import TaskResponse, QueueResult, TaskStatus
+from .survey_schemas import SurveyData
+
+__all__ = [
+    'TaskResponse',
+    'QueueResult',
+    'TaskStatus',
+    'SurveyData',
+]
