2f5739c43b64c1680c15d680b7d7cc36f6696b5e
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-20 03:14:36 +0300

    refactor: update primary key definition in survey_template model

--- app/models/survey_template.py
+++ app/models/survey_template.py
@@ -6,7 +6,7 @@ class SurveyTemplate(Model):
     """
     Model representing a survey template.
     """
-    id = fields.IntField(pk=True, description='Primary key')
+    id = fields.IntField(primary_key=True, description='Primary key')
     name = fields.CharField(max_length=255, unique=True, description='Name of the survey template')
     json_content = fields.JSONField(description='JSON content of the survey template')
 
