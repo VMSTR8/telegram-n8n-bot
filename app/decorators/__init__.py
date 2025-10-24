653e499eff43db0628ded130cc96f6f616057a94
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-24 04:21:37 +0300

    fix: remove unused FastAPIValidate import from module exports

--- app/decorators/__init__.py
+++ app/decorators/__init__.py
@@ -1,10 +1,8 @@
 from .auth import AuthDecorators
 from .validate import CallsignDecorators, SurveyCreationDecorators
-from .fastapi_validate import FastAPIValidate
 
 __all__ = [
     'AuthDecorators',
     'CallsignDecorators',
-    'SurveyCreationDecorators',
-    'FastAPIValidate'
+    'SurveyCreationDecorators'
 ]
