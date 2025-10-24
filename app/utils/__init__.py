4e750facf3245c64bf56db1669d8e176956bcf2f
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-24 04:21:44 +0300

    fix: add send_callsign_validation_error to module exports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,9 +3,11 @@ from .validators import (
     validate_datetime_format, 
     ValidationResult
 )
+from .mesages import send_callsign_validation_error
 
 __all__ = [
     'validate_callsign_format',
     'validate_datetime_format',
-    'ValidationResult'
+    'ValidationResult',
+    'send_callsign_validation_error'
 ]
