f58cb231d33ba3c09f95644fa9557cf767d2e9b0
Maxim Vinokurov <vmstr8@yandex.ru>
2025-10-28 21:20:33 +0300

    feat: add escape_markdown function to module exports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -4,10 +4,12 @@ from .validators import (
     ValidationResult
 )
 from .mesages import send_callsign_validation_error
+from .markdown import escape_markdown
 
 __all__ = [
     'validate_callsign_format',
     'validate_datetime_format',
     'ValidationResult',
+    'escape_markdown',
     'send_callsign_validation_error'
 ]
