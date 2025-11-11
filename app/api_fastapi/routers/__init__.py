f2df9d6ea33d1b6d166567161e0a4ee5995f90ac
Maxim Vinokurov <vmstr8@yandex.ru>
2025-11-11 23:52:28 +0300

    feat: add recaptcha verification router to the application

--- app/api_fastapi/routers/__init__.py
+++ app/api_fastapi/routers/__init__.py
@@ -1,7 +1,9 @@
 from .n8n_webhook import n8n_webhook_router
 from .telegram_webhook import telegram_webhook_router
+from .recaptcha_verification import recaptcha_verification_router
 
 __all__ = [
     'telegram_webhook_router',
     'n8n_webhook_router',
+    'recaptcha_verification_router',
 ]
